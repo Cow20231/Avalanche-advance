c8c690e54fbb6cd4e21d77e2148609b4e316a832
Cow20231 <142592160+Cow20231@users.noreply.github.com>
2024-03-08 10:29:54 +0530

    Update ERC20.sol

--- AVAX-advance-1/ERC20.sol
+++ AVAX-advance-1/ERC20.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "PAVITRA";
-    string public symbol = "PAV";
+    string public name = "Akash";
+    string public symbol = "Coin";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
